72d3db8c9657de4d00b4960636508716340757fa
Wio-kun <edvval09@vgs.nfk.no>
2023-01-25 13:53:18 +0100

    w

--- index.php
+++ index.php
@@ -2,20 +2,11 @@
 <html>
 
     <head>
-        <title> Welcome to not Chungus Toasters </title>
+
     </head>
 
     <body>
-        
-        <h1> 8 ball thingy </h1>
 
-        <?php
-        
-        include "8ball.php";
-
-        ?>
-
-        <h3> Ta dah </h3>
     </body>
 
 </html>
---
+++ web_for_fun.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+
+    <head>
+        <title>Character Review</title>
+        <link rel="stylesheet" href="style_for_fun.css">
+    </head>
+
+    <body>
+        <div class="alt">
+            <div class="header">
+                <?php
+                    echo "<h1 class='overskrift'>Character Review</h1>"
+                ?>
+            </div>
+            <div class="home">
+                <div class="linker">
+                    <?php
+                        echo "<a href='https://www.twitch.tv/iazoiw'>twitch.tv/iazoiw</a>";
+                    ?>
+                </div>
+                <div class="innhold">
+                    <?php
+                        echo "<img class='gyro'src='gyro.png' alt='Gyro'>";
+                    ?>
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
