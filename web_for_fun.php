216a8f080f6f392a7153b5ac3ae047b3cce7aa6f
Wio-kun <edvval09@vgs.nfk.no>
2023-01-26 10:51:39 +0100

    ?

--- web_for_fun.php
+++ web_for_fun.php
@@ -10,22 +10,26 @@
         <div class="alt">
             <div class="header">
                 <?php
-                    echo "<h1 class='overskrift'>Character Review</h1>"
+                    echo "<h1 class='overskrift'>Iazoiw</h1>"
                 ?>
             </div>
             <div class="home">
                 <div class="linker">
                     <?php
-                        echo "<a href='https://www.twitch.tv/iazoiw'>twitch.tv/iazoiw</a>";
+                        echo "<a href='https://www.twitch.tv/iazoiw'><button><h1>twitch.tv/Iazoiw</h1></button></a>";
+
+                        for ($i=0; $i < 101; $i++) {
+                            echo "$i viewer/s <br>";
+                        }
                     ?>
                 </div>
                 <div class="innhold">
                     <?php
-                        echo "<img class='gyro'src='gyro.png' alt='Gyro'>";
+                        echo "<div class='iazoiw_div'><img class='iazoiw'src='pineapple.jpg' alt='Iazoiw'></div>";
+                        echo "<img class='beach'src='beach.jpg' alt='Beach'>";
                     ?>
                 </div>
             </div>
         </div>
     </body>
-
 </html>
